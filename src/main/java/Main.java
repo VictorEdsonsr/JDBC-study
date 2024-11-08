8e7e4af445eb51cf4ba56d326be07820e4b9a791
VictorEdsonsr <edvictorsr@gmail.com>
2024-11-08 14:11:44 -0400

    class created to connect the db with jdbc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import db.DB;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
+        Connection conn = DB.getConnection();
 
+
+        System.out.println(conn);
     }
 }
