098a0585bf08963c7ec758cf9fab2df171267759
VictorEdsonsr <edvictorsr@gmail.com>
2024-11-11 17:41:49 -0400

    create helper methods to close sql properties

--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -26,6 +24,22 @@ public class DB {
     public static void closeConnection(){
         try{
             conn.close();
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatment(Statement statement) {
+        try{
+            statement.close();
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet) {
+        try{
+            resultSet.close();
         }catch (SQLException e){
             throw new DbException(e.getMessage());
         }
